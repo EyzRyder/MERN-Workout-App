7964f82fabdf7b4b665939025a2ee80f83b6adf7
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-12-26 17:06:44 -0300

    added signup function

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -11,6 +13,24 @@ const userSchema = new Schema({
         type: String,
         require: true
     }
-}, { timestamps: true });
+});
+
+//static singup method
+
+userSchema.statics.signup = async function (email, password) {
+
+    const exists = await this.findOne({ email })
+    
+    if (exists) {
+        throw new Error('Email already in use');
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema);
