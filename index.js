357fb387a64735ac15f74d67c43d648daa223dd2
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-01-08 11:12:24 -0300

    update backend

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require("cors");
 const mongoose = require('mongoose');
+
 const workoutRoutes = require('./routes/workoutRoutes');
 const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 const PORT = (process.env.PORT || 3000);
 
@@ -11,14 +14,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, './frontend/dist')));
+
+app.get("/", function (_, res) {
+    res.sendFile(
+        path.join(__dirname, ''),
+        function (err) {
+            res.status(500).send(err);
+        }
+    );
+});
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
-})
+});
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
 
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
 mongoose.set("strictQuery", true);
 
